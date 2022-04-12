890915f680c7e4ef4e6cd128fbc431e788a00f5c
gattino09 <Gattino09@Air-di-Eleonora.homenet.telecomitalia.it>
2022-04-12 15:13:26 +0200

    Updated

--- FlakeOS/Download_files/Download.js
+++ FlakeOS/Download_files/Download.js
@@ -4,6 +4,6 @@ setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
 {return false;}
 function onPageLoad()
 {loadMozillaCSS('Download_files/DownloadMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');Widget.onload();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
+detectBrowser();Widget.onload();fixAllIEPNGs('Media/transparent.gif');fixupAllIEPNGBGs();performPostEffectsFixups()}
 function onPageUnload()
 {Widget.onunload();}
