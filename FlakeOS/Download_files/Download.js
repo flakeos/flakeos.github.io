898598c44bd1f7f708c923647410273b569238e4
BuildTools <unconfigured@null.spigotmc.org>
2022-04-24 23:38:03 +0200

    Updated the Website

--- FlakeOS/Download_files/Download.js
+++
@@ -1,9 +0,0 @@
-// Created by iWeb 3.0.4 local-build-20220412
-
-setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
-{return false;}
-function onPageLoad()
-{loadMozillaCSS('Download_files/DownloadMoz.css')
-detectBrowser();Widget.onload();fixAllIEPNGs('Media/transparent.gif');fixupAllIEPNGBGs();performPostEffectsFixups()}
-function onPageUnload()
-{Widget.onunload();}
